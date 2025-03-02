d340df52809ef5f21db837dad86e23c9059f5bd3
seriojaa <kaliashvilisergi@gmail.com>
2025-03-03 02:44:17 +0400

    Second Task Andersen Lab

---
+++ script.js
@@ -0,0 +1,13 @@
+// const simpleCalculator = (a, b) => {
+//     if (b !== undefined) {
+//         return a + b;
+//     } else {
+//         return (c) => a + c;
+//     }
+// };
+
+// console.log(simpleCalculator(1, 2))
+
+// console.log(simpleCalculator(1)(2))
+
+window.alert()
