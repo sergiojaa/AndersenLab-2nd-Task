56ab12b5e19997385887cff8a7e631ea88532c20
seriojaa <kaliashvilisergi@gmail.com>
2025-03-03 12:04:14 +0400

    Complete 2nd week homework

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-// const simpleCalculator = (a, b) => {
-//     if (b !== undefined) {
-//         return a + b;
-//     } else {
-//         return (c) => a + c;
-//     }
-// };
+//1st task
+const simpleCalculator = (a, b) => {
+    if (b !== undefined) {
+        return a + b;
+    } else {
+        return (c) => a + c;
+    }
+};
 
-// console.log(simpleCalculator(1, 2))
+console.log(simpleCalculator(1, 2))
+console.log(simpleCalculator(1)(2))
 
-// console.log(simpleCalculator(1)(2))
+//2nd task
+const myFilter = (array, callback, thisArg) => {
+    const result = [];
+    array.forEach((element, index, arr) => {
+        if (callback.call(thisArg, element, index, arr)) {
+            result.push(element);
+        }
+    });
+    return result;
+};
+const numbers = [1, 2, 3, 4, 5];
+const evenNumbers = myFilter(numbers, num => num % 2 === 0);
+console.log(evenNumbers);
 
-window.alert()
+//3rd task
+[window.alert, window.prompt, window.confirm] = [window.confirm, window.alert, window.prompt];
+
